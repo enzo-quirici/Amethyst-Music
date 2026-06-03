7de821d9bad3478d2373392a9e84d8e055d94a81
Enzo Quirici <enzo.pro@ik.me>
2026-06-03 21:20:12 +0200

    added config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_HOST', '127.0.0.1');
+define('DB_PORT', '3306');
+define('DB_NAME', 'purple_music');
+define('DB_USER', 'purple_music_user');
+define('DB_PASS', 'DB_Password');
+define('MUSIC_DIR',  __DIR__ . '/music');
+define('COVER_DIR',  __DIR__ . '/covers');
